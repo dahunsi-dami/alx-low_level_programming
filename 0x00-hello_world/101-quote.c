5442d04cced68454e214377d62ae512f98a6f7e8
Dre <gbemigadare@gmail.com>
2023-10-05 13:17:53 -0700

    Write program to print string before new line to standard error without using printf, puts & their functions

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints
+ * a string with a new line to
+ * standard error.
+ *
+ * Return: to 1. That is, indicate an
+ * error. Or print to standard error.
+ */
+
+int main(void)
+{
+	char *gstring = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	int length;
+
+	length = strlen(gstring);
+
+	write(STDERR_FILENO, gstring, length);
+	return (1);
+}
