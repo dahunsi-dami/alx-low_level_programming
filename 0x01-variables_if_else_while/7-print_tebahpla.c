09686284faa9f29bc182060bf4157f58b219cc62
Dre <gbemigadare@gmail.com>
2023-10-06 16:05:39 -0700

    Print low case alphabet in reverse with n using putchar func

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - print low case alphabet with new line
+ *
+ * Description: You can only use the putchar function and twice max
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s;
+
+	s = 'z';
+
+	while (s > 'a')
+	{
+		putchar(s);
+		s--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
