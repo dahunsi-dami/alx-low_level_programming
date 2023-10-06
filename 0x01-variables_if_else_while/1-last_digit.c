dfe5aa98c76c7739a950182099971d1a0dec96ef
Dre <gbemigadare@gmail.com>
2023-10-06 12:29:53 -0700

    Print if n is >5, =0, or <6 and not 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,20 +13,23 @@
 int main(void)
 {
 	int n;
+	int ldgt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	if (n > 0)
+	ldgt = n % 10;
+
+	if (n > 5)
 	{
-		printf("%d is positive\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ldgt);
 	} else if (n == 0)
 	{
-		printf("%d is zero\n", n);
-	} else if (n < 0)
+		printf("Last digit of %d is %d and is 0\n", n, ldgt);
+	} else if (n < 6 && n != 0)
 	{
-		printf("%d is negative\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldgt);
 	} else
 	{
 		printf("Please enter a number");
