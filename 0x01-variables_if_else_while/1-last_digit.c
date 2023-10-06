7d664fc7a8171e9676d61e074fe1288aa5456170
Dre <gbemigadare@gmail.com>
2023-10-06 12:33:41 -0700

    Change var in conditional to last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,13 +21,13 @@ int main(void)
 
 	ldgt = n % 10;
 
-	if (n > 5)
+	if (ldgt > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ldgt);
-	} else if (n == 0)
+	} else if (ldgt == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ldgt);
-	} else if (n < 6 && n != 0)
+	} else if (ldgt < 6 && ldgt != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldgt);
 	} else
