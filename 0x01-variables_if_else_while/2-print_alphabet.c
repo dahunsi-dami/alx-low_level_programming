e42104ef806a0ef5dda888b9f7b062a665fb457a
Dre <gbemigadare@gmail.com>
2023-10-06 15:01:10 -0700

    Print alphabet in lowcase using putchar only twice & new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Prints alphabet in lowercase followed by new linei
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char s;
+
+	s = 'a';
+
+	while (s <= 'z')
+	{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
