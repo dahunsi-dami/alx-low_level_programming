ed5abaec85a913448b33f9f74596c9d0816f5abf
Dre <gbemigadare@gmail.com>
2023-10-10 02:58:19 -0700

    Create new dir for functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
+
 /**
- * main - Entry point
+ * main - entry point to print string
  *
- * Description: Prints "_putchar" followed by new line
- *
- * Return: Always 0 (Success)
+ * Return: always 0 to denote success
  */
 
 int main(void)
 {
-	char *a = "_putchar";
-	int length;
-	int i;
-
-	length = strlen(a);
-
-	for (i = 0; i <= length; i++)
-	{
-		putchar(a[i]);
-	}
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
