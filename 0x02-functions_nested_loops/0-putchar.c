be019e061d8af01d23be9b6944a9dc0f6ae87e48
Dre <gbemigadare@gmail.com>
2023-10-10 00:02:18 -0700

    Add a README.md file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *a = "_putchar";
+
+	putchar(a);
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains C code to learn and practice functions and nested loops
