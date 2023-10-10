e2a6a3adb1c3afe8ebde0b296522a21f2c0e86d1
Dre <gbemigadare@gmail.com>
2023-10-10 10:19:48 -0700

    Save progress.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the 9 times table starting with 0
+ * 
+ * Return: unnecessary as char type in prototype is void
+ */
+
+void times_table(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 1;
+
+	while (a <= 9)
+	{
+		while (b <= 9)
+		{
+			b = b * c
+			-putchar(b + '0')
+			b++
+		}
+
+}
