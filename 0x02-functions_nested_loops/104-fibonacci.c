81d8133d8ad5dca8ec1da103d9e6d213b5fb0f6f
Dre <gbemigadare@gmail.com>
2023-10-11 19:41:27 -0700

    Print 1st 98 numbers of fibonacci sequence with splitting technique to handle large numbers.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,47 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - prints 1st 98 Fibonacci numbers followed by new line
- *
  * Return: always 0 (success)
  */
-
 int main(void)
 {
-	unsigned long a = 1;
-	unsigned long b = 2;
+	unsigned long a = 1, b = 2, s = 0, a_hf1, a_hf2, b_hf1, b_hf2, hf1, hf2;
 	int i;
-	unsigned long sum;
 
 	printf("%lu, %lu, ", a, b);
 
-	for (i = 0; i <= 95; i++)
+	for (i = 0; i < 90; i++)
 	{
-		sum = a + b;
-		if (i == 95)
-		{
-			printf("%lu", sum);
-		}
-		else
-		{
-			printf("%lu, ", sum);
-		}
+		s = a + b;
+
+		printf("%lu, ", s);
 		a = b;
-		b = sum;
+		b = s;
 	}
+	a_hf1 = a / 10000000000;
+	a_hf2 = a % 10000000000;
+	b_hf1 = b / 10000000000;
+	b_hf2 = b % 10000000000;
+
+	for (i = 90; i < 96; i++)
+	{
+		hf1 = a_hf1 + b_hf1;
+		hf2 = a_hf2 + b_hf2;
+		if (hf2 > 9999999999)
+		{
+			hf1 += 1;
+			hf2 %= 10000000000;
+		}
+		printf("%lu%lu", hf1, hf2);
+		if (i != 95)
+			printf(", ");
+		a_hf1 = b_hf1;
+		a_hf2 = b_hf2;
+		b_hf1 = hf1;
+		b_hf2 = hf2;
+	}
+
 	printf("\n");
 	return (0);
 }
