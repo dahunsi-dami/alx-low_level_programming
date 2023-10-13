6d57d7b5c0e6b6e24e85dfc8b2a89c165e2016a2
Dre <gbemigadare@gmail.com>
2023-10-12 17:58:15 -0700

    Print 1 to 100 & do the Fizz-Buzz test.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print 1-100 Fizz for 3 multiples, Buzz for 5, FizzBuzz for both
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((a == 3) || (a % 3 == 0))
+		{
+			printf("Fizz ");
+		}
+		else if (((a % 5 == 0) || (a % 5 == 0)) && a != 99)
+		{
+			printf("Buzz ");
+		}
+		else if (((a % 5 == 0) || (a % 5 == 0)) && a == 99)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
