8f4548cb2955f521bd9bfcf245e3aa89e89988b6
Dre <gbemigadare@gmail.com>
2023-10-16 10:15:35 -0700

    Write func that updates value that int n points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - Resets given argument to 98
+ *
+ * Return: unnecessary as function type is void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
