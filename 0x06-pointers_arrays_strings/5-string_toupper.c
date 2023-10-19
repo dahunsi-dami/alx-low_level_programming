8cead991fa20e93b971d89bdb007779bc041bcf2
Dre <gbemigadare@gmail.com>
2023-10-19 15:25:00 -0700

    Write func that changes lowcase letter to upcase.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *string_toupper - changes all lowcase letters of string to upcase.
+ * @str: the string whose lowcase will be switched to upcase.
+ * Return: always 0 (success).
+ */
+
+char *string_toupper(char *str)
+{
+	int i, length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length ; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (str);
+}
