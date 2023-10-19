8f45f220140975a66ee72316f5234c728f3a1207
Dre <gbemigadare@gmail.com>
2023-10-19 05:46:22 -0700

    Write func to concat n bytes of a str with another str.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strncat - function that appends n bytes of src to dest.
+ * @dest: the string to be appended to.
+ * @src: the string to be appended.
+ * @n: bytes of src to be appended.
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int k, lendest = 0;
+
+	while (dest[lendest] != '\0')
+	{
+		lendest++;
+	}
+
+	for (k = 0; src[k] && k < n; k++)
+	{
+		dest[lendest + k] = src[k];
+	}
+
+	return (dest);
+}
