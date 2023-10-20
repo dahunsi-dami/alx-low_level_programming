b08aaab1c54e4752c2a827910b7280f878e0728f
Dre <gbemigadare@gmail.com>
2023-10-20 09:57:31 -0700

    Print last byte of buffer with for loop.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,7 +8,7 @@
  */
 void print_buffer(char *b, int size)
 {
-	int a, c;
+	int a, e, c;
 
 	a = 0;
 
@@ -19,30 +19,32 @@ void print_buffer(char *b, int size)
 	}
 	while (a < size)
 	{
-		int e = (size - a < 10) ? size - a : 10;
-
+		e = size - a < 10 ? size - a : 10;
 		printf("%08x: ", a);
 
-		for (c = 0; c < e; c++)
+		for (c = 0; c < 10; c++)
 		{
-			printf("%02x", *(b + a + c));
+			if (c < e)
+				printf("%02x", *(b + a + c));
+			else
+				printf(" ");
 			if (c % 2)
 			{
 				printf(" ");
 			}
 		}
-		for (c = 0; c < 10 - e; c++)
-			printf("   ");
 		for (c = 0; c < e; c++)
 		{
-			char d = *(b + a + c);
+			int d = *(b + a + c);
 
-			if (d < 32 || d > 126)
+			if (d < 32 || d > 132)
 			{
 				d = '.';
 			}
 			printf("%c", d);
 		}
+		if (c == e - 1)
+			printf("%02x", *(b + a + c));
 		printf("\n");
 		a += 10;
 	}
