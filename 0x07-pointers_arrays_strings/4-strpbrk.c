d6f004510844e471ca3260d6f9980909b2a0bea3
Dre <gbemigadare@gmail.com>
2023-10-23 19:10:14 -0700

    Write a function that searches a string for any set of bytes in accept.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any set of bytes.
+ * @s: the string to be searched for bytes in accept.
+ * @accept: the string where chars will be cross-checked.
+ *
+ * Return: pointer to byte in s that's in accept else NULL.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + 1);
+			}
+		}
+	}
+	return (NULL);
+}
