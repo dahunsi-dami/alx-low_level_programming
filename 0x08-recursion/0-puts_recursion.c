10f9783d3f05f84fd17d75ba13428ca85db9ae19
Dre <gbemigadare@gmail.com>
2023-10-25 07:45:20 -0700

    Edit short description to align with betty guidelines.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion: function that prints a string before a new line.
+ * _puts_recursion - function that prints a string before a new line.
  * @s: the string to be printed before a new line.
  * Return: unnecessary as function prototype is void.
  */
