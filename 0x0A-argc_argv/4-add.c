d57782690e0c34c0646b08276c0ddfe28909b436
Dre <gbemigadare@gmail.com>
2023-10-27 06:54:30 -0700

    Write prog that adds arguments after program name, 0 if no numbers passed, Error if one of arguments isn't digit.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints sum of two arguments after 1st arg.
+ * @argc: number of arguments, including program name.
+ * @argv: 1D array of arguments passed.
+ *
+ * Return: always 0 (success) or 1 (error).
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int num = atoi(argv[i]);
+
+			if (num != 0)
+			{
+				sum += num;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
