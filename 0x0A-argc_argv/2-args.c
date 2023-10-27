c92358842bcae0d32573d099ad54f876cea7e55a
Dre <gbemigadare@gmail.com>
2023-10-27 04:50:41 -0700

    Write a prog that prints all arguments it gets per line, including prog name.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints given character c to stdout of 1.
+ *
+ * @c: character to be printed
+ *
+ * Return: returns 1 if successful and -1 if error's encountered
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - prints number of arguments minus program name.
+ * @argc: number of arguments, including program name.
+ * @argv: 1D array of arguments passed.
+ *
+ * Return: always 0 (to denote success).
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			_putchar(argv[i][j]);
+		}
+		_putchar('\n');
+	}
+
+	return (0);
+}
