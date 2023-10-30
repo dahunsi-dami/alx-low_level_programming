5a315f70b08ec9061ed9660bdcfe575e885c6e94
Dre <gbemigadare@gmail.com>
2023-10-30 05:48:46 -0700

    Write func that returns pointer to dynamically alloc 2D array.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - returns pointer to 2D array of integers
+ * intialized to 0.
+ * @width: the width of the 2D array.
+ * @height: the height of the 2D array.
+ *
+ * Return: pointer to 2D array, else NULL if fail.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ar, x, y;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ar = malloc(sizeof(int) * height);
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (y = 0; y < height; y++)
+	{
+		ar[y] = malloc(sizeof(int) * width);
+
+		if (ar[y] == NULL)
+		{
+			return (NULL);
+		}
+
+		for (x = 0; x < width; x++)
+		{
+			ar[y][x] = 0;
+		}
+	}
+	return (ar);
+}
