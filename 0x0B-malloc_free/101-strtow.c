72959626d73393a06ca5501749ae5d346661abbf
Dre <gbemigadare@gmail.com>
2023-11-01 21:12:31 -0700

    Break down the entire logic into function modules for word count, array freeing, and splitting.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,48 @@
 #include <stdlib.h>
-#include <string.h>
+/**
+ * wordcounter - function that counts number of words in string
+ * @str: the string that has the words in it.
+ *
+ * Return: integer (number of words counted).
+ */
+int wordcounter(char *str)
+{
+	int w;
+
+	while (*str != '\0')
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			while (*str != ' ' && *str != '\0')
+				str++;
+			w++;
+		}
+	}
+	return (w);
+}
+
+/**
+ * release_array - function that frees all dynamically alloc arrays.
+ * @arry: the array to be released.
+ * @i: the index of the array.
+ *
+ * Return: unnecessary as function prototype is void.
+ */
+void release_array(char **arry, int i)
+{
+	if (arry != NULL && i != 0)
+	{
+		while (i >= 0)
+		{
+			free(arry[i]);
+			i--;
+		}
+		free(arry);
+	}
+}
+
 /**
  * strtow - prints space-separated strings into words.
  * @str: the string to be splitted.
@@ -9,47 +52,37 @@
 char **strtow(char *str)
 {
 	char **nstrg;
-	int i, j, k, word_len = 0, worder = 0;
+	int i, j, k, wc, wi;
 
-	if (str == NULL || strcmp(str, "") == 0)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-			worder++;
-	}
-	if (worder == 0)
-	{
-		nstrg = malloc(sizeof(char *));
-		if (nstrg == NULL)
-			return (NULL);
-		nstrg[0] = NULL;
-		return (nstrg);
-	}
-	nstrg = malloc(sizeof(char *) * (worder + 1));
-	if (nstrg == NULL)
-		return (NULL);
-	for (i = 0, j = 0; i < worder; i++)
-	{
-		while (str[j] == ' ')
-			j++;
-		word_len = 0;
+	wc = wordcounter(str);
 
-		while (str[j + word_len] != ' ' && str[j + word_len] != '\0')
-			word_len++;
-		nstrg[i] = malloc((word_len + 1) * sizeof(char));
-		if (nstrg[i] == NULL)
+	nstrg = malloc(sizeof(char *) * (wc + 1));
+	if (nstrg == NULL || wc == 0)
+		return (NULL);
+	for (i = j = 0; i < wc; i++)
+	{
+		for (wi = j; str[wi] != '\0'; wi++)
 		{
-			for (k = 0; k < i; k++)
-				free(nstrg[k]);
-			free(nstrg);
-			return (NULL);
+			if (str[wi] == ' ')
+				j++;
+			if (str[wi] != ' ' && (str[wi + 1] ==
+						' ' || str[wi + 1] == '\0'))
+			{
+				nstrg[i] = malloc((wi - j + 2) * sizeof(char));
+				if (nstrg[i] == NULL)
+				{
+					release_array(nstrg, i);
+					return (NULL);
+				}
+				break;
+			}
 		}
-		for (k = 0; k < word_len; k++)
-			nstrg[i][k] = str[j + k];
-		nstrg[i][k] = '\0';
 
-		j += word_len;
+		for (k = 0; j <=  wi; j++, k++)
+			nstrg[i][k] = str[j];
+		nstrg[i][k] = '\0';
 	}
 	nstrg[i] = NULL;
 
