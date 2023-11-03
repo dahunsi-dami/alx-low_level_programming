c598a90ba848e6811fbe9e041027b8d643601087
Dre <gbemigadare@gmail.com>
2023-11-02 17:36:48 -0700

    Write a func that allocs memory for an array using malloc, but memory set to 0 to mimic calloc.

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array with malloc & memory set to 0.
+ * @nmemb: the number of elements to be in the array.
+ * @size: the size of each elements in bytes.
+ *
+ * Return: return pointer to allocated memory, else NULL.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *nstrg;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	nstrg = malloc(size * nmemb);
+
+	if (nstrg == NULL)
+		return (NULL);
+
+	for (i = 0; nstrg[i] != '\0'; i++)
+		nstrg[i] = 0;
+
+	nstrg[i] = '\0';
+
+	return (nstrg);
+
+}
