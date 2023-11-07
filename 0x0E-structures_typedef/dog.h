9e6a5feab974dd6e851dd7bfcfbde8fb88c0ab4f
Dre <gbemigadare@gmail.com>
2023-11-06 17:25:21 -0800

    Make new dir and def new type struct dog with 3 elements.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - new type struct dog with 3 elements.
+ * @name: The first member and name of the dog.
+ * @age: The 2nd member and age of the dog.
+ * @owner: The 3rd member and ownder of the dog.
+ *
+ * Description: This struct type is a record of dogs
+ * with items like the name of the dog, its age, and
+ * its owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
