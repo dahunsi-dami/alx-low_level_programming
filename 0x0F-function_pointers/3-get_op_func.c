2874a0ce8d51931063bc087ba398675e5138d6e8
Dre <gbemigadare@gmail.com>
2023-11-08 21:09:07 -0800

    Adjust indentation in 3-get_op_func.c.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,14 +9,14 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
+       	op_t ops[] = {
+	       	{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-	};
+       	};
 	int i;
 
 	for (i = 0; i < 5; i++)
