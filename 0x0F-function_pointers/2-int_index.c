4a75c18b2dada71d120a94b96ce36ac41180eca8
Dre <gbemigadare@gmail.com>
2023-11-08 18:20:28 -0800

    Write func that searches for int where cmp func != 0.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - searches for an integer
+ * Description: returns the index of the 1st element
+ * for which compare func doesn't return 0.
+ * @array: the array containing the elements.
+ * @size: the size of the array or number of elements.
+ * @cmp: function pointer to each func in 2-main.c
+ *
+ * Return: -1 if no element matches, else the int
+ * that matches.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,11 +4,7 @@
 
 int _putchar(char c);
 void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
-void print_name(char *name, void (*f)(char *));
-int main(void);
-void print_elem(int elem);
-void print_elem_hex(int elem);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
