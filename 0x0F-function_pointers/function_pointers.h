4190273a6abdb7000534612c7bd8064004caace4
Dre <gbemigadare@gmail.com>
2023-11-08 18:03:11 -0800

    Write func that execs func given as param on all array elements.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
@@ -6,5 +7,8 @@ void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
 int main(void);
+void print_elem(int elem);
+void print_elem_hex(int elem);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
