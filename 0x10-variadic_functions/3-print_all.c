fedad9444676f53b8ec15beb5b80b80e71e1e371
Dre <gbemigadare@gmail.com>
2023-11-09 18:42:53 -0800

    Write a func that prints anything: char, int, float, string.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ * print_all - a function that prints anything.
+ * @format: a list of types of arguments passed to func.
+ *
+ * Return: unnecessary as function returns void type.
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *sep = "", *s;
+
+	va_list types;
+
+	va_start(types, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(types, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(types, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(types, double));
+					break;
+				case 's':
+					s = va_arg(types, char *);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s%s", sep, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ",";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(types);
+}
