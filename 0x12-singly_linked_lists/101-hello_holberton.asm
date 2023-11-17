c41ea222788c2246d62987534bac03c3964f30c5
Dre <gbemigadare@gmail.com>
2023-11-16 19:58:15 -0800

    Write a 64 bit prog in assembly that prints a string before new line with printf().

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,17 @@
+section .data
+	message db "Hello, Holberton", 10, 0	; newline and null terminator
 
-          global    main         ; entry point for c-compiler
-          extern    printf       ; will be borrowed from c-library
-                                 ; printf(char* format, data)
-                                 ; printf(rdi, rsi)
+section .text
+	global main	; entry point for c-compiler
+	extern printf	; will be borrowed from c-library
+			; printf(char* format, data)
+			; printf(rdi, rsi)
 
-          section   .text
-main:   
-          lea       rdi, [rel message]   ; the format is the message!
-                                         ; no need for rsi
-                           ; printf("Hello, Holberton");
-                   ; or (alternative):
-          lea       rdi, [rel fmts]
-          lea       rsi, [rel message]
-                           ; printf("%s", "Hello, Holberton");
-              
-          mov       al, 0        ; no SSE regs used
-          call      printf
+main:
+	mov rdi, message	; the format is the message
+	xor rax, rax		; clear the RAX register for no SSE regs used
+	call printf
 
-          mov       rax, 0x3C    ; system call for exit
-          xor       rdi, rdi     ; exit code 0
-          syscall                ; invoke operating system to exit
-
-          section   .data
-message:  db        "Hello, Holberton"; don't forget to terminate
-   fmts:  db        "%s",0       ; format fpr printing string
+	mov rax, 60		; system call for exit
+	xor rdi, rdi		; exit code 0
+	syscall			; invoke operating system to exit
