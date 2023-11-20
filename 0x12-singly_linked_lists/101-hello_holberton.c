996fdc405e9349d27621d4a4e437b08e2a340fd3
Dre <gbemigadare@gmail.com>
2023-11-20 03:14:57 -0800

    Create new directory with README to practice more singly linked lists.

---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function that prints a given string.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("Hello, Holberton\n");
+
+	return (0);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory is to practice more singly linked lists.
