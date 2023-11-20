d1170128c28b235b6193e036acfff951461b2c5a
Dre <gbemigadare@gmail.com>
2023-11-20 03:19:36 -0800

    Create header file lists.h and import custom putchar function.

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+#endif
