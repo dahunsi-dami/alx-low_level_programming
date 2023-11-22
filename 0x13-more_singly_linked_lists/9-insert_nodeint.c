55ffaab24130105d287cd2f40e52ee46312a4526
Dre <gbemigadare@gmail.com>
2023-11-21 20:51:56 -0800

    Change condition in while statement to just truthy.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode = NULL;
 	backnode = NULL;
 
-	while (nextnode != NULL)
+	while (nextnode)
 	{
 		if (i == idx)
 		{
