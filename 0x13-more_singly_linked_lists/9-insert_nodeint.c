96ce8a6d3e2686307e6d2833f4668be1fff85a2c
Dre <gbemigadare@gmail.com>
2023-11-21 20:44:11 -0800

    Initialize previous node and next node to NULL.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newnode, *backnode, *nextnode;
 
 	nextnode = *head;
+	newnode = NULL;
+	backnode = NULL;
 
 	while (nextnode != NULL)
 	{
