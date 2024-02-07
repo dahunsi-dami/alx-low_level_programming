0d7414e423d4883b97aca9197e2095a944d3bb7a
Dre <gbemigadare@gmail.com>
2024-02-07 12:52:44 +0100

    Create table like DigitalOcean Sample Hash Table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,10 +7,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hasht;
+	unsigned long int i;
 
-	hasht = malloc(size);
+	hasht = malloc(sizeof(hash_table_t));
 	if (hasht == NULL)
 		return (NULL);
+
+	hasht->size = size;
+	hasht->array = calloc(hasht->size, sizeof(hash_node_t *));
+
+	for (i = 0; i < hasht->size; i++)
+		hasht->array[i] = NULL;
 
 	return (hasht);
 }
